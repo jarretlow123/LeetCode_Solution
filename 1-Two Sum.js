8c004bc1f611452f79bffc3a9ab57ec7d3f7eee7
Kah Xuan <jarretlow123@gmail.com>
2023-12-29 23:38:18 +0800

    new file:   1-Two Sum.js

---
+++ 1-Two Sum.js
@@ -0,0 +1,32 @@
+// Time complexity: O(n)
+// Space complexity: O(1)
+// hash table
+
+var twoSum = function(nums, target) {
+    const map = new Map();
+
+    for (var i = 0; i < nums.length; i++) {
+
+        var complement = target - nums[i]
+
+        if (map.has(complement)) {
+            return [i,map.get(complement)]
+        }
+
+        map.set(nums[i],i)
+    }
+};
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+// brute force
+
+var twoSum = function(nums, target) {
+    let map=new Map();
+    for (var i = 0; i < nums.length; i++) {
+        if (map.has(target - nums[i])) {
+            return [i,map.get(target - nums[i])]
+        }
+        map.set(nums[i],i)
+    }
+};
